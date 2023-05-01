1645e5329641289ea32341ab1f007b3b28c50185
Valeriya Chuvak <chva5000@gmail.com>
2023-05-01 20:40:48 +0300

    feat: add keyboard functionality

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -14,6 +14,7 @@ let textarea = document.createElement('textarea');
 textarea.classList.add('textarea');
 textarea.setAttribute('rows', '5');
 textarea.setAttribute('cols', '50');
+textarea.setAttribute('autofocus', '');
 let keyboard = document.createElement('div');
 keyboard.classList.add('keyboard');
 
@@ -21,30 +22,28 @@ let keyboardRow1En = document.createElement('div');
 let keyboardRow1Rus = document.createElement('div');
 keyboardRow1En.classList.add('keyboard__row');
 keyboardRow1Rus.classList.add('keyboard__row');
-if (!keyboardRow1En.classList.contains('keyboard__row_inactive') && !keyboardRow1Rus.classList.contains('keyboard__row_inactive')) {
-  keyboardRow1Rus.classList.add('keyboard__row_inactive');
-}
+keyboardRow1En.classList.add('keyboard__row_active');
 let button = document.createElement('div');
 let keyboardRow2En = document.createElement('div');
 let keyboardRow2Rus = document.createElement('div');
 keyboardRow2En.classList.add('keyboard__row');
 keyboardRow2Rus.classList.add('keyboard__row');
-keyboardRow2Rus.classList.add('keyboard__row_inactive');
+keyboardRow2En.classList.add('keyboard__row_active');
 let keyboardRow3En = document.createElement('div');
 let keyboardRow3Rus = document.createElement('div');
 keyboardRow3En.classList.add('keyboard__row');
 keyboardRow3Rus.classList.add('keyboard__row');
-keyboardRow3Rus.classList.add('keyboard__row_inactive');
+keyboardRow3En.classList.add('keyboard__row_active');
 let keyboardRow4En = document.createElement('div');
 let keyboardRow4Rus = document.createElement('div');
 keyboardRow4En.classList.add('keyboard__row');
 keyboardRow4Rus.classList.add('keyboard__row');
-keyboardRow4Rus.classList.add('keyboard__row_inactive');
+keyboardRow4En.classList.add('keyboard__row_active');
 let keyboardRow5En = document.createElement('div');
 let keyboardRow5Rus = document.createElement('div');
 keyboardRow5En.classList.add('keyboard__row');
 keyboardRow5Rus.classList.add('keyboard__row');
-keyboardRow5Rus.classList.add('keyboard__row_inactive');
+keyboardRow5En.classList.add('keyboard__row_active');
 
 let row1ContentEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
 let row1ContentEnUp = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'];
@@ -125,8 +124,8 @@ let addRow4 = (lang, row) => {
 addRow4(row4ContentEn, keyboardRow4En)
 addRow4(row4ContentRus, keyboardRow4Rus)
 
-let row5ContentEn = ['Ctrl', '⊞', 'Alt', '', 'Alt', 'ᐊ', 'ᐁ', 'ᐅ', 'Ctrl'];
-let row5ContentRus = ['Ctrl', '⊞', 'Alt', '', 'Alt', 'ᐊ', 'ᐁ', 'ᐅ', 'Ctrl'];
+let row5ContentEn = ['Ctrl', '⊞', 'Alt', ' ', 'Alt', 'ᐊ', 'ᐁ', 'ᐅ', 'Ctrl'];
+let row5ContentRus = ['Ctrl', '⊞', 'Alt', ' ', 'Alt', 'ᐊ', 'ᐁ', 'ᐅ', 'Ctrl'];
 let row5Keys = ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 let addRow5 = (lang, row) => {
   for (let i = 0; i < row5ContentEn.length; i++) {
@@ -172,8 +171,17 @@ body.prepend(footer);
 body.prepend(main);
 body.prepend(header);
 
+let keyboardRows = document.querySelectorAll('.keyboard__row');
+let start;
+let end;
+
 keyboard.addEventListener('click', (event) => {
   let target = event.target;
+
+  textarea.focus();
+  start = textarea.selectionStart;
+  end = textarea.selectionEnd;
+
   if (target.classList.contains('keyboard__button')) {
     target.classList.add('keyboard__button_active');
     setTimeout(() => {
@@ -181,33 +189,129 @@ keyboard.addEventListener('click', (event) => {
     }, 200);
 
     if (target.classList.contains('AltLeft')) {
-      let keyboardRows = document.querySelectorAll('.keyboard__row');
       keyboardRows.forEach((row) => {
-        row.classList.toggle('keyboard__row_inactive')
+        row.classList.toggle('keyboard__row_active')
       })
     }
-  
-    if (!target.classList.contains('Backspace') & !target.classList.contains('Tab') & !target.classList.contains('Delete') & !target.classList.contains('CapsLock') & !target.classList.contains('Enter') & !target.classList.contains('ShiftLeft') & !target.classList.contains('ShiftRight') & !target.classList.contains('AltLeft') & !target.classList.contains('AltRight') & !target.classList.contains('MetaLeft') & !target.classList.contains('ControlLeft') & !target.classList.contains('ControlRight') & !target.classList.contains('ArrowUp') & !target.classList.contains('ArrowLeft') & !target.classList.contains('ArrowRight') & !target.classList.contains('ArrowDown')) {
-      textarea.textContent += `${target.textContent}`;
+
+    if (target.classList.contains('Backspace')) {
+      textarea.textContent = textarea.textContent.slice(0, start - 1) + textarea.textContent.slice(end);
+      textarea.selectionStart = start = start - 1;
+      textarea.selectionEnd = end = end - 1;
+    } else if (target.classList.contains('Tab')) {
+      textarea.textContent = textarea.textContent.slice(0, start) + '   ' + textarea.textContent.slice(end);
+      textarea.selectionStart = start = start + 3;
+      textarea.selectionEnd = end = end + 3;
+    } else if (target.classList.contains('Delete')) {
+      textarea.textContent = textarea.textContent.slice(0, start) + textarea.textContent.slice(end + 1);
+      textarea.selectionStart = start;
+      textarea.selectionEnd = end;
+    } else if (target.classList.contains('Enter')) {
+      textarea.textContent = textarea.textContent.slice(0, start) + '\n' + textarea.textContent.slice(end);
+      textarea.selectionStart = start = start + 1;
+      textarea.selectionEnd = end = end + 1;
+    } else if (target.classList.contains('ArrowLeft')) {
+      textarea.selectionStart = start = start - 1;
+      textarea.selectionEnd = end = end - 1;
+    } else if (target.classList.contains('ArrowRight')) {
+      textarea.selectionStart = start = start + 1;
+      textarea.selectionEnd = end = end + 1;
+    } else if (target.classList.contains('ArrowUp')) {
+      let arr = textarea.textContent.split('\n');
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        sum += arr[i].length;
+        if (sum >= textarea.selectionStart) {
+          textarea.selectionStart = start = start - arr[i-1].length - 1;
+          textarea.selectionEnd = end = end - arr[i-1].length - 1;
+          break;
+        }
+      }
+    } else if (target.classList.contains('ArrowDown')) {
+      let arr = textarea.textContent.split('\n');
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        sum += arr[i].length;
+        if (sum >= textarea.selectionStart) {
+          textarea.selectionStart = start = start + arr[i].length + 1;
+          textarea.selectionEnd = end = end + arr[i].length + 1;
+          break;
+        }
+      }
+    } else if (!target.classList.contains('Backspace') & !target.classList.contains('Tab') & !target.classList.contains('Delete') & !target.classList.contains('CapsLock') & !target.classList.contains('Enter') & !target.classList.contains('ShiftLeft') & !target.classList.contains('ShiftRight') & !target.classList.contains('AltLeft') & !target.classList.contains('AltRight') & !target.classList.contains('MetaLeft') & !target.classList.contains('ControlLeft') & !target.classList.contains('ControlRight') & !target.classList.contains('ArrowUp') & !target.classList.contains('ArrowLeft') & !target.classList.contains('ArrowRight') & !target.classList.contains('ArrowDown')) {
+      textarea.textContent = textarea.textContent.slice(0, start) + `${target.textContent}` + textarea.textContent.slice(end);
+      textarea.selectionStart = start = start + 1;
+      textarea.selectionEnd = end = end + 1;
     }
   }
 })
 
 let buttons = document.querySelectorAll('.keyboard__button');
 let rowKeys = [...row1Keys, ...row2Keys, ...row3Keys, ...row4Keys, ...row5Keys]
+
 document.addEventListener('keydown', (event) => {
+  start = textarea.selectionStart;
+  end = textarea.selectionEnd;
 
   if (event.shiftKey &&  event.altKey) {
     let keyboardRows = document.querySelectorAll('.keyboard__row');
     keyboardRows.forEach((row) => {
-      row.classList.toggle('keyboard__row_inactive')
+      row.classList.toggle('keyboard__row_active');
     })
   }
 
   for (let i = 0; i < rowKeys.length; i++) {
     if (event.code === `${rowKeys[i]}`) {
-      if (event.code !== 'Backspace' & event.code !== 'Tab' & event.code !== 'Delete' & event.code !== 'CapsLock' & event.code !== 'Enter' & event.code !== 'ShiftLeft' & event.code !== 'ShiftRight' & event.code !== 'AltLeft' & event.code !== 'AltRight' & event.code !== 'MetaLeft' & event.code !== 'ControlLeft' & event.code !== 'ControlRight' & event.code !== 'ArrowUp' & event.code !== 'ArrowLeft' & event.code !== 'ArrowRight' & event.code !== 'ArrowDown') {
-        textarea.textContent += `${event.key}`;
+      event.preventDefault();
+
+      if (event.code === 'Backspace') {
+        textarea.textContent = textarea.textContent.slice(0, start - 1) + textarea.textContent.slice(end);
+        textarea.selectionStart = start = start - 1;
+        textarea.selectionEnd = end = end - 1;
+      } else if (event.code === 'Tab') {
+        textarea.textContent = textarea.textContent.slice(0, start) + '   ' + textarea.textContent.slice(end);
+        textarea.selectionStart = start = start + 3;
+        textarea.selectionEnd = end = end + 3;
+      } else if (event.code === 'Delete') {
+        textarea.textContent = textarea.textContent.slice(0, start) + textarea.textContent.slice(end + 1);
+        textarea.selectionStart = start;
+        textarea.selectionEnd = end;
+      } else if (event.code === 'Enter') {
+        textarea.textContent = textarea.textContent.slice(0, start) + '\n' + textarea.textContent.slice(end);
+        textarea.selectionStart = start = start + 1;
+        textarea.selectionEnd = end = end + 1;
+      } else if (event.code === 'ArrowLeft') {
+        textarea.selectionStart = start = start - 1;
+        textarea.selectionEnd = end = end - 1;
+      } else if (event.code === 'ArrowRight') {
+        textarea.selectionStart = start = start + 1;
+        textarea.selectionEnd = end = end + 1;
+      } else if (event.code === 'ArrowUp') {
+        let arr = textarea.textContent.split('\n');
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+          sum += arr[i].length;
+          if (sum >= textarea.selectionStart) {
+            textarea.selectionStart = start = start - arr[i-1].length - 1;
+            textarea.selectionEnd = end = end - arr[i-1].length - 1;
+            break;
+          }
+        }
+      } else if (event.code === 'ArrowDown') {
+        let arr = textarea.textContent.split('\n');
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+          sum += arr[i].length;
+          if (sum >= textarea.selectionStart) {
+            textarea.selectionStart = start = start + arr[i].length + 1;
+            textarea.selectionEnd = end = end + arr[i].length + 1;
+            break;
+          }
+        }
+      } else if (event.code !== 'Backspace' & event.code !== 'Tab' & event.code !== 'Delete' & event.code !== 'CapsLock' & event.code !== 'Enter' & event.code !== 'ShiftLeft' & event.code !== 'ShiftRight' & event.code !== 'AltLeft' & event.code !== 'AltRight' & event.code !== 'MetaLeft' & event.code !== 'ControlLeft' & event.code !== 'ControlRight' & event.code !== 'ArrowUp' & event.code !== 'ArrowLeft' & event.code !== 'ArrowRight' & event.code !== 'ArrowDown') {
+        textarea.textContent = textarea.textContent.slice(0, start) + `${event.key}` + textarea.textContent.slice(end);
+        textarea.selectionStart = start = start + 1;
+        textarea.selectionEnd = end = end + 1;
       }
       for (let btn of buttons) {
         if (btn.classList.contains(`${rowKeys[i]}`)) {
